907a5d0c60b0a331882b45e2c81afc9d595a0c84
Valera Savchenko <ivalera.develop@gmail.com>
2023-11-16 02:43:50 +0300

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -1,53 +1,154 @@
-const INPUT_ERROR = 'Введите текст!';
+const INPUT_INITIAL = '';
+const INCORRECT_INDEX = -1;
 
-const TASK_FORM_HIGH = document.querySelector('#form-high-task');
-const TASK_FORM_LOW = document.querySelector('#form-low-task');
+const ERRORS = {
+    ERROR_CHANGE_STATUS : "Error, there is no such task to change the status!",
+    ERROR_ADD_TASK : "Error, incorrect input in add task!",
+    ERROR_DELETE_TASK : "Error, not found task for delete!",
+    ERROR_INPUT_TASK : "Error, you have not entered a task!",
+    ERROR_PRIORITY_INPUT : "Error, not priority for task!"
+};
+
+const STATUS = {
+    TO_DO : "To do",
+    IN_PROGRESS : "In progress",
+    DONE : "Done",
+};
+const PRIORITY = {
+    HIGH : 'high',
+    LOW : 'low',
+    MEDIUM: 'medium'
+};
+let toDo = [ 
+    {name: 'Создать задачу.', status: STATUS.TO_DO, priority : PRIORITY.HIGH}, 
+    {name: 'Пойти домой.', status: STATUS.DONE, priority : PRIORITY.LOW},
+    {name: 'Программировать каждый день.', status: STATUS.DONE, priority : PRIORITY.HIGH},
+    {name: 'Зайти к другу.', status: STATUS.TO_DO, priority : PRIORITY.LOW},
+    {name: 'Хлеб', status: STATUS.TO_DO, priority : PRIORITY.LOW}    
+];
+
+const TASK_FORMS = document.querySelectorAll('form');
 const TASK_INPUT_HIGH = document.querySelector('#todo-high-input');
 const TASK_INPUT_LOW = document.querySelector('#todo-low-input');
+const TASK_CARD = document.querySelector(".todo__card");
 
 const log = (value) => console.log(value);
 
-TASK_FORM_HIGH.addEventListener('submit', addDataTaskHigh);
-TASK_FORM_LOW.addEventListener('submit', addDataTaskLow);
+log(toDo);
 
-function createTask(name, parentElement){
+showTasks(PRIORITY.HIGH, 0);
+showTasks(PRIORITY.LOW, 1);
+TASK_CARD.addEventListener("click", removeElementTask);
+TASK_CARD.addEventListener("click", changeElementStatus);
+
+TASK_FORMS.forEach((form) => {
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const formId = form.id;
+        const taskInput = form['task'].value;
+
+        if(formId === "form-high-task"){
+            createTask(form, taskInput, STATUS.TO_DO);
+            addTask(taskInput, PRIORITY.HIGH);
+        }
+        if(formId === "form-low-task"){
+            createTask(form, taskInput, STATUS.TO_DO);
+            addTask(taskInput, PRIORITY.LOW);
+        }
+        form['task'].value = INPUT_INITIAL;
+    });
+});
+
+function showTasks(priority, formPriority){
+    const priorityTask = toDo.filter(element => element.priority === priority);
+    if(!priorityTask.length){
+        log('Задач с высоким приоритетом нет!');
+    }else{
+        priorityTask.forEach(element => createTask(TASK_FORMS[formPriority], element.name,  element.status));
+    }
+} 
+
+function addTask(task, taskPriority){
+    if(!task){
+        console.log(ERRORS.ERROR_INPUT_TASK);
+    }else{
+        const newTask = {name: task, status: STATUS.TO_DO, priority: taskPriority};
+        toDo.push(newTask);
+        log(toDo);
+    }
+}
+
+function createTask(priorityForm, nameTask, status){
     const taskBlock = document.createElement('div');
-    taskBlock.className = 'todo__task';
     const taskCheckbox = document.createElement('input');
-    taskCheckbox.type = 'checkbox';
-    taskCheckbox.className = 'todo__status';
     const taskText = document.createElement('p');
-    taskText.textContent = name;
-    var btnTask = document.createElement('button');
-    btnTask.id = 'btn-close';
-    btnTask.className = 'btn btn__close';
+    const taskDelete = document.createElement('button');
+    taskBlock.className = 'todo__task';
+    taskCheckbox.type = 'checkbox';
+    if(status === STATUS.DONE){
+        taskCheckbox.checked = true;
+    }
+    taskCheckbox.className = 'todo__status';
+    taskText.className = "task__text";
+    taskText.textContent = nameTask;
+    taskDelete.className = 'btn btn__delete';
     taskBlock.appendChild(taskCheckbox);
     taskBlock.appendChild(taskText);
-    taskBlock.appendChild(btnTask);
-    parentElement.parentNode.insertBefore(taskBlock, parentElement.nextSibling);
+    taskBlock.appendChild(taskDelete);
+    priorityForm.parentNode.insertBefore(taskBlock, priorityForm.nextSibling);
 }
 
-function addDataTaskHigh(event){
-    event.preventDefault();
-    const data = new FormData(event.target);
-  console.log([...data.entries()]);
-    log(TASK_FORM_HIGH);
-    log(event);
-    if(!TASK_INPUT_HIGH.value){
-        TASK_INPUT_HIGH.placeholder = INPUT_ERROR;
-        return;
+function deleteTask(task){
+    if(!task){
+        console.log(ERRORS.ERROR_INPUT_TASK);
+    }else{
+        const indexTask = toDo.findIndex(element => element.name === task);
+        if(indexTask != INCORRECT_INDEX){
+            toDo.splice(indexTask, 1);
+            log(toDo);
+        }else{
+            console.log(ERRORS.ERROR_DELETE_TASK);
+        }
     }
-    createTask(TASK_INPUT_HIGH.value, TASK_FORM_HIGH);
-    TASK_INPUT_HIGH.value = "";
 }
 
-function addDataTaskLow(event){
-    event.preventDefault();
-    log(event);
-    if(!TASK_INPUT_LOW.value){
-        TASK_INPUT_LOW.placeholder = INPUT_ERROR;
-        return;
+function removeElementTask(event){
+    const clickedResult = event.target;
+    log(clickedResult);
+    if(clickedResult.classList.contains('btn__delete')){
+        const task = event.target.parentElement.querySelector('.task__text').innerText;
+        deleteTask(task);
+        TASK_CARD.removeChild(event.target.parentElement)
     }
-    createTask(TASK_INPUT_LOW.value, TASK_FORM_LOW);
-    TASK_INPUT_LOW.value = "";
+}
+
+function chanegeStatus(task){
+    if(!task){
+        console.log(ERRORS.ERROR_INPUT_TASK);
+    }else{
+        const foundTask = toDo.find(element => element.name === task);
+        if(!foundTask){
+            console.log(ERRORS.ERROR_CHANGE_STATUS);
+        }else{
+            log(foundTask.status);
+            if(foundTask.status === STATUS.TO_DO){
+                foundTask.status = STATUS.DONE;
+            }else{
+                foundTask.status = STATUS.TO_DO;
+            }
+            
+            log(toDo);
+        }
+    }
+}
+
+function changeElementStatus(event){
+    const clickedResult = event.target;
+    log(clickedResult);
+    if(clickedResult.classList.contains('todo__status')){
+        const task = event.target.parentElement.querySelector('.task__text').innerText;
+        log(task);
+        chanegeStatus(task);
+    }
 }
